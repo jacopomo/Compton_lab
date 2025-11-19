af397e9de154d84592d8f4a42272546bd9ccaf81
jacopomo <jacopo.omodei@gmail.com>
2025-11-19 10:28:52 +0100

    Updated thetaMC

--- dtheta_MC.py
+++ dtheta_MC.py
@@ -4,27 +4,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 import scipy.interpolate as interp
+import time
+
+######## Constanti ########
+### Geometria:
+L = 11 #Lunghezza collimatore [cm]
+DSP = L/2 + 1.5 # Distanza sorgente - plastico [cm]
+DBC = 47 # Distanza bersaglio - cristallo [cm]
+RC = 2.9 # Raggio del cristallo [cm]
+SAAC = np.arctan(RC/DBC) # Semi-apertura angolare del cristallo [rad]
+THETA_C = 20 # Angolo al quale si trova il cristallo [gradi]
+THETA_C = THETA_C*np.pi/180 # Convertilo il radianti [rad]
+
+### Fisica
+ALPHA = 1/137 # Costante di struttura fine
+ME = 511 # Massa dell'elettrone [keV]
+RE = 1/(ALPHA*ME) # Raggio classico elettrone
+E1, E2 = 1173.240, 1332.508 # Energie dei fotoni
+
+### Config
+THETA_MIN, THETA_MAX = -np.pi, np.pi # Theta min e max
+THETA_MESH = np.linspace(THETA_MIN, THETA_MAX, 10000) # Theta mesh
+N_MC = int(1e7) # Num samples
+np.random.seed(42) # Seed
+
+######## Classi ########
 
 
-### Constants
-# Lunghezza collimatore
-L = 11 #cm
-
-# Distanza sorgente - plastico
-DSP = L/2 + 1.5 #cm
-
-# Costante di struttura fine
-ALPHA = 1/137
-
-# Massa elettrone
-ME = 511 #keV
-
-# Raggio classico elettrone
-RE = 1/(ALPHA*ME)
-
-
-
-
+######## Funzioni ########
 
 def lambdaratio(theta, E):
     """""
@@ -39,7 +46,6 @@ def lambdaratio(theta, E):
 
     epsilon = E/ME
     return 1/(1+(epsilon*(1-np.cos(theta))))
-
 
 def kn(theta, E):
     """"" Klein Nishima formula
@@ -56,27 +62,57 @@ def kn(theta, E):
 
     return 0.5*(RE**2)*(lr)**2*(lr + (lr)**-1 - (np.sin(theta))**2)
 
-def campiona_kn(theta, E, N):
+def campiona_kn(theta_mesh, E, N):
     """"" Campionamento della K-N usando tecniche numeriche
+
+    Parametri:
+    theta_mesh: Un mesh di theta da esplorare (linspace theta min-max)
+    E: Energia del fotone incidente
+    N: Numero di campionamenti
     """""
-    kn_norm = kn(theta, E) / integrate.quad(kn, -np.pi, np.pi, args=(E))[0] #Klein Nishima normalizzata 0-1
-    cdf = np.cumsum(kn_norm) * (theta[1] - theta[0])  
+
+    kn_norm = kn(theta_mesh, E) / integrate.quad(kn, -np.pi, np.pi, args=(E))[0] #Klein Nishima normalizzata 0-1
+    cdf = np.cumsum(kn_norm) * (theta_mesh[1] - theta_mesh[0])  
     cdf = cdf / cdf[-1] 
-    cdf=interp.CubicSpline(theta, cdf)
+    inv_cdf = interp.CubicSpline(cdf, theta_mesh)
     u = np.random.uniform(0,1, N)
+    x = inv_cdf(u)
+    return x
 
-    return cdf
+def scatter(E):
+    theta_in = np.random.uniform(-4.75, 4.75, N_MC)*np.pi/180
+    r = np.arctan(theta_in ) * DSP
+    theta_out = theta_in + campiona_kn(THETA_MESH, E, 1)
+    pass
 
+######## Monte-Carlo ########
+start = time.time()
 #### Distribuzione angolare uniforme
-theta_in = np.random.uniform(-4.75, 4.75, 10000)*np.pi/180
-theta_random = np.random.uniform(-180, 180, 10000)*np.pi/180
-theta_mesh = np.linspace(-np.pi, np.pi, 10000)
+theta_in = np.random.uniform(-4.75, 4.75, N_MC)*np.pi/180 # radianti
 
 # Posizione del fotone sul PMT1
 r = np.arctan(theta_in ) * DSP
 
+# Aggiorna l'angolo 
+theta_out = theta_in + campiona_kn(THETA_MESH, E1, N_MC)
 
-plt.scatter(theta_mesh, campiona_kn(theta_mesh, 1170,1))
+#plt.hist(theta_out, bins=60, color="red", label="theta out")
+#plt.legend()
+#plt.show()
+
+thetas_accepted=[]
+for i in theta_out:
+    if (i>THETA_C-SAAC) and (i<THETA_C + SAAC):
+        thetas_accepted.append(i)
+thetas_accepted = np.array(thetas_accepted)*180/np.pi
+
+print(f'\nAbbiamo utilizzato solo {round(100 * len(thetas_accepted)/N_MC,2)}% dei dati')
+print(f'Angoli che vediamo: [{round(min(thetas_accepted),2)}, {round(max(thetas_accepted),2)}] gradi, con il cristallo centrato a {THETA_C*180/np.pi} gradi')
+plt.hist(thetas_accepted)
+
+
+
+# Timing
+end = time.time()
+print(f'\nTempo impiegato: {round(end - start,2)}s')
 plt.show()
-
-
