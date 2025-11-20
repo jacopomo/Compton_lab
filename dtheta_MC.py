25dbd4ddee46caf661478cdb01cb459fbddbf498
jacopomo <jacopo.omodei@gmail.com>
2025-11-20 16:43:12 +0100

    Aggiunto 30 gradi e pulito mc

--- dtheta_MC.py
+++ dtheta_MC.py
@@ -13,8 +13,9 @@ DSP = L/2 + 1.5 # Distanza sorgente - plastico [cm]
 DBC = 47 # Distanza bersaglio - cristallo [cm]
 RC = 2.9 # Raggio del cristallo [cm]
 SAAC = np.arctan(RC/DBC) # Semi-apertura angolare del cristallo [rad]
-THETA_C = 20 # Angolo al quale si trova il cristallo [gradi]
-THETA_C = THETA_C*np.pi/180 # Convertilo il radianti [rad]
+PHI = 20  # Angolo al quale si trova il cristallo [gradi]
+FLUSSO = 2258 # Fotoni al secondo
+
 
 ### Fisica
 ALPHA = 1/137 # Costante di struttura fine
@@ -25,11 +26,9 @@ E1, E2 = 1173.240, 1332.508 # Energie dei fotoni
 ### Config
 THETA_MIN, THETA_MAX = -np.pi, np.pi # Theta min e max
 THETA_MESH = np.linspace(THETA_MIN, THETA_MAX, 10000) # Theta mesh
-N_MC = int(1e7) # Num samples
+N_MC = int(1e4) # Num samples
+STAT_DES = 10000 # Statistica desiderata per l'esperimento
 np.random.seed(42) # Seed
-
-######## Classi ########
-
 
 ######## Funzioni ########
 
@@ -69,6 +68,8 @@ def campiona_kn(theta_mesh, E, N):
     theta_mesh: Un mesh di theta da esplorare (linspace theta min-max)
     E: Energia del fotone incidente
     N: Numero di campionamenti
+
+    Returns: Numpy array di N angoli (in radianti) distribuiti secondo la KN normalizzata
     """""
 
     kn_norm = kn(theta_mesh, E) / integrate.quad(kn, -np.pi, np.pi, args=(E))[0] #Klein Nishima normalizzata 0-1
@@ -79,40 +80,84 @@ def campiona_kn(theta_mesh, E, N):
     x = inv_cdf(u)
     return x
 
-def scatter(E):
-    theta_in = np.random.uniform(-4.75, 4.75, N_MC)*np.pi/180
-    r = np.arctan(theta_in ) * DSP
-    theta_out = theta_in + campiona_kn(THETA_MESH, E, 1)
-    pass
+def calcola_angolo_cristallo(phi, dbc, dsp):
+    """"" Trova l'angolo tra la "sorgente" e il centro del detector, usato ai fini dell'accettanza
+
+    Parametri:
+    phi: angolo in gradi sul goiniometro del cristallo
+    dbc: distanza bersaglio-cristallo
+    dsp: distanza sorgente-plastico
+
+    Returns: l'angolo in radianti
+    """""
+    phi = np.radians(phi)
+    l = np.sqrt(dsp**2+dbc**2-(2*dsp*dbc*np.cos(np.pi-phi)))
+    angolo = np.arccos((dsp-(dbc*np.cos(np.pi-phi)))/l)
+    return angolo
+
+def distribuzione_sorgente(n, type=0):
+    """"" Funzione che seleziona la distribuzione della sorgente di Co
+
+    Parametri: 
+    n: Numero di fotoni da generare
+    type: Tipo di sorgente. 0 = uniforme, 1 = BOH
+
+    Returns:
+    array di angoli in radianti generati dalla distribuzione
+    """""
+    if type==0:
+        theta_in = np.radians(np.random.uniform(-4.75, 4.75, n)) # radianti
+    else:
+        print("devo ancora implementare altre cose")
+    return theta_in
+
+def scatter(E, n):
+    """"" Funzione che calcola l'angolo di scattering Compton
+
+    Parametri: 
+    E: Energia del fotone incidente [keV]
+    n: Numero di fotoni da scatterare
+
+    Returns:
+    theta_scattered: L'angolo di scattering Compton (radianti)
+    theta_total: L'angolo calcolato dalla sorgente ai fini di contare se arriva sul cristallo (radianti)
+    """""
+    theta_in = distribuzione_sorgente(n, type=0) # radianti (uniforme)
+    theta_scattered = campiona_kn(THETA_MESH, E, n) # radianti
+    theta_total = theta_in + theta_scattered
+    return theta_scattered, theta_total
 
 ######## Monte-Carlo ########
 start = time.time()
-#### Distribuzione angolare uniforme
-theta_in = np.random.uniform(-4.75, 4.75, N_MC)*np.pi/180 # radianti
 
-# Posizione del fotone sul PMT1
-r = np.arctan(theta_in ) * DSP
+theta_scattered, theta_total = scatter(E1, N_MC)
 
-# Aggiorna l'angolo 
-theta_out = theta_in + campiona_kn(THETA_MESH, E1, N_MC)
+plt.hist(theta_total, bins=80)
+plt.axvline(calcola_angolo_cristallo(PHI,DBC,DSP), color="red")
+plt.axvline(calcola_angolo_cristallo(PHI,DBC,DSP)-SAAC, color="red", linestyle="--", alpha=0.7)
+plt.axvline(calcola_angolo_cristallo(PHI,DBC,DSP)+SAAC, color="red", linestyle="--", alpha=0.7)
 
-#plt.hist(theta_out, bins=60, color="red", label="theta out")
-#plt.legend()
-#plt.show()
+plt.show()
 
 thetas_accepted=[]
-for i in theta_out:
-    if (i>THETA_C-SAAC) and (i<THETA_C + SAAC):
+for i in theta_total:
+    if (i>np.radians(PHI)-SAAC) and (i<np.radians(PHI) + SAAC):
         thetas_accepted.append(i)
-thetas_accepted = np.array(thetas_accepted)*180/np.pi
+thetas_accepted = np.degrees(np.array(thetas_accepted))
 
-print(f'\nAbbiamo utilizzato solo {round(100 * len(thetas_accepted)/N_MC,2)}% dei dati')
-print(f'Angoli che vediamo: [{round(min(thetas_accepted),2)}, {round(max(thetas_accepted),2)}] gradi, con il cristallo centrato a {THETA_C*180/np.pi} gradi')
+
+fotoni_visti = len(thetas_accepted)
+print(f'\n ========== RISULTATI ==========')
+print(f'Abbiamo visto {round(fotoni_visti,2)} fotoni su {int(N_MC)}, ({round(100 * fotoni_visti/N_MC,2)}%)')
+print(f'Per vederne {STAT_DES} servierebbero {round(STAT_DES*N_MC/(FLUSSO*60*fotoni_visti),2)} minuti')
+print(f'Angoli che vediamo: [{round(min(thetas_accepted),2)}, {round(max(thetas_accepted),2)}] gradi, con il cristallo centrato a {PHI} gradi')
+print(f'Delta theta = {round(max(thetas_accepted)-min(thetas_accepted),2)} gradi')
+print(f' ===============================\n')
 plt.hist(thetas_accepted)
 
 
 
 # Timing
 end = time.time()
-print(f'\nTempo impiegato: {round(end - start,2)}s')
+print(f'Tempo impiegato: {round(end - start,2)}s')
 plt.show()
