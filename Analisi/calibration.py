83afaab40b8cb0e5288f144015cbeb2e82ce0108
FerrariLuca04 <lucaferrariroggio@gmail.com>
2025-11-20 21:22:02 +0100

    First reorganization

--- Analisi/calibration.py
+++ Analisi/calibration.py
@@ -2,11 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from concurrent.futures import ProcessPoolExecutor
-from scipy.optimize import curve_fit
+#from scipy.optimize import curve_fit
 import os
 import time
 
 import utils as u
+
+
+
+    
 
 #Setupping directories and files' path
 tot_path = os.path.dirname(__file__)
@@ -17,13 +21,8 @@ cfg = np.loadtxt(config_file,
                  unpack=True)
 
 calibration_dir = os.path.join(tot_path, cfg[0])
+output_dir = os.path.join(tot_path, cfg[1])
 calibration_files = u.searchfiles(calibration_dir, "dat")
-
-file_map = {}
-for f in calibration_files:
-    base = os.path.basename(f)
-    stem = os.path.splitext(base)[0]
-    file_map[stem] = f
 
 
 #Generate a dict for configuration values for calibration files
@@ -31,12 +30,23 @@ config_cal_file = os.path.join(calibration_dir, "config.txt")
 config_cal_dict = u.gen_dict(config_cal_file)
 
 #Assign unbinned data to each element's dict
-for element, _dict in config_cal_dict.items():
-    path = file_map.get(element)
-    if path is None:
-        continue
+for element,_dict in config_cal_dict.items():
+    path = next((x for x in calibration_files if element in x), None)
     unbinned_data = u.unbin(path)
     _dict["data"] = unbinned_data
+
+#Fitting function
+def fit_calibration(dict):
+    data = dict["data"]
+    range = dict["range"]
+    count, bins = np.histogram(data, 330, range=range)
+
+    plt.figure(figsize=(6,4))
+    plt.stairs(count,bins)
+    plt.savefig(os.path.join(output_dir, "Graph/output.jpg"), format="jpg", dpi=600, bbox_inches='tight')
+    plt.close()
+
+fit_calibration(config_cal_dict["Cs137"])
 
 
 
