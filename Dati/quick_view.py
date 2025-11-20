87f1784a24adcd42855dc1a23849e502bc5847fc
Giuseppe Tomaselli <peppe@MAC-428DB9.station>
2025-11-20 21:45:16 +0100

    Analisi background coincidenze

--- Dati/quick_view.py
+++ Dati/quick_view.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 file_name = input("Nome file -> ")
-file = r"Dati/Calibration/" + file_name
+file = r"Dati/Calibration/Background/" + file_name
 
 resize_factor = int(input("Resize factor dell'istogramma -> "))
 bins_tot = 8192
@@ -13,6 +13,9 @@ dat = np.loadtxt(file, dtype=int, unpack=True)
 bin_indices = np.arange(dat.size, dtype=int)
 unbinned = np.repeat(bin_indices, dat)
 
-
+plt.title(f'Spettro Gamma in Scala Logaritmica')
+plt.xlabel(f'Canale')
+plt.ylabel('Conteggi (Scala Logaritmica)')
+plt.yscale('log')
 count = plt.hist(unbinned, bins=num_bins, histtype='step')
 plt.show()
