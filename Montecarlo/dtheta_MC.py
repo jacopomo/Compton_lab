780a1359487e87d8cd414d372366d2a410adb5e8
jacopomo <jacopo.omodei@gmail.com>
2025-11-23 03:15:08 +0100

    Aggiunto figure, riscritto montecarlo

--- Montecarlo/dtheta_MC.py
+++ Montecarlo/dtheta_MC.py
@@ -1,5 +1,3 @@
-### WORK IN PROGRESS!!!!!!!!
-
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
@@ -9,12 +7,14 @@ import time
 ######## Constanti ########
 ### Geometria:
 L = 11 #Lunghezza collimatore [cm]
-DSP = L/2 + 1.5 # Distanza sorgente - plastico [cm]
+SAACOLL = np.degrees(np.arctan(2/L)) # Semi-apertura angolare del collimatore [gradi]
+DSP = 1.5 # Distanza sorgente - plastico [cm]
 DBC = 47 # Distanza bersaglio - cristallo [cm]
 RC = 2.9 # Raggio del cristallo [cm]
 SAAC = np.arctan(RC/DBC) # Semi-apertura angolare del cristallo [rad]
-PHI = 15  # Angolo al quale si trova il cristallo [gradi]
 FLUSSO = 2258 # Fotoni al secondo
+
+PHI = 20  # Angolo al quale si trova il cristallo [gradi]
 
 
 ### Fisica
@@ -26,63 +26,98 @@ E1, E2 = 1173.240, 1332.508 # Energie dei fotoni
 ### Config
 THETA_MIN, THETA_MAX = -np.pi, np.pi # Theta min e max
 THETA_MESH = np.linspace(THETA_MIN, THETA_MAX, 10000) # Theta mesh
-N_MC = int(1e6) # Num samples
 STAT_DES = 10000 # Statistica desiderata per l'esperimento
 np.random.seed(42) # Seed
+
+N_MC = int(1e7) # Num samples (MASSIMO 5e7 SE NON VUOI FAR DIVENTARE IL TUO COMPUTER UN TERMOSIFONE)
 
 ######## Classi ########
 
 class Superficie:
-    def __init__(self, raggio, posizione=(0,0,0), angolo=0): # Passa gradi
-        self.posizione = posizione
+    def __init__(self, raggio, centro=(0,0,0), angolo=0): # Passa gradi
+        self.centro = np.array(centro)
         self.angolo = np.radians(angolo)
         self.raggio = raggio
 
-    def genera_uniforme(self, n):
+    def normal(self):
+        if np.linalg.norm(self.centro)==0:
+            normal = np.array([0,0,1])
+        else:
+            normal = self.centro / np.linalg.norm(self.centro)
+        return normal
+    
+    def pos_sul_piano_unif(self, n, debug_graph=False):
         thetas = np.random.uniform(0,2*np.pi, n)
         rs = self.raggio * np.sqrt(np.random.uniform(0,1,n))
-        distribuzione_sorgente(n, type=0)
-        pass
+        fx = self.centro[0] + (rs*np.sin(thetas))
+        fy = self.centro[1] + (rs*np.cos(thetas)*np.cos(self.angolo))
+        fz = self.centro[2] + (rs*np.cos(thetas)*np.sin(self.angolo)) 
+        if debug_graph:
+            fig = plt.figure(figsize=(12, 12))
+            ax = fig.add_subplot(projection='3d')
+            ax.scatter(fx,fy,fz) 
+            plt.show()
+        return fx, fy, fz
+
 
 class Fotone:
-    def __init__(self, energia, posizione=[0,0,0], direzione=[0,0]): #Passa gradi phi psi
+    def __init__(self, energia, px, py, pz, phi, psi): #Passa gradi phi psi
         self.energia = energia
-        self.posizione = np.array(posizione)
-        self.direzione = np.radians(np.array(direzione))
+        self.px = px
+        self.py = py
+        self.pz = pz
+        self.phi = np.radians(phi)
+        self.psi = np.radians(psi)
+
+    def calcola_int(self, superficie, debug_graph=False, scatter_compton=False):
+        p = np.stack((self.px, self.py, self.pz), axis=-1)
+        phi, psi = self.phi, self.psi
+        centro = superficie.centro
+        normal = superficie.normal()
+
+        if scatter_compton:
+            scatter_angle = campiona_kn(THETA_MESH, self.energia, len(self.px))
+            delta = np.random.uniform(-np.pi/2,np.pi/2, len(self.px)) 
+            phi, psi = phi + (scatter_angle*np.cos(delta)), psi + (scatter_angle*np.sin(delta))
+
+        dx, dy, dz = np.sin(psi), np.sin(phi)*np.cos(psi), np.cos(phi)*np.cos(psi)
+        d = np.stack((dx, dy, dz), axis=-1)
+        num, denom = (centro-p) @ normal, np.sum(d * normal, axis=-1)
+
+        parallel = np.isclose(denom, 0.0, atol=1e-8)
+        t = np.full_like(denom, np.nan, dtype=float)
+
+        valid = ~parallel
+        t[valid] = num[valid] / denom[valid]
+        
+        forward = (~np.isnan(t)) & (t >= -1e-8)
+        if not np.any(forward):
+            return None, None, None, None, None
     
-    def scatter_uniforme(self):
-        pass
+        pts = p + np.expand_dims(t, axis=-1) * d
+        pts = pts[forward]
+        phi = phi[forward]
+        psi = psi[forward]
+        if scatter_compton:
+            scatter_angle = scatter_angle[forward]
 
-    def scatter_compton(self):
-        pass
-    
-    def calcola_int(self, superficie):
-        posizione = self.posizione
-        direzione = self.direzione
-        centro = np.array(superficie.posizione)
-        if np.linalg.norm(centro)==0:
-            normal = np.array([1,0,0])
-        else:
-            normal = centro / np.linalg.norm(centro)
-
-        if direzione[0]==0:
-            direzione[0]==0.01
-        if direzione[1] == 0:
-            direzione[1] == 0.01
-
-        y_int =  ((normal[1]*centro[1]) + (normal[2]*(centro[2] -posizione[2] - (posizione[1]/np.tan(direzione[0])))))/((normal[1]) + (normal[2]/np.tan(direzione[0])))
-        z_int = ((y_int-posizione[1])/np.tan(direzione[0])) + posizione[2]
-        x_int = ((z_int-posizione[2]) * np.tan(direzione[1])) + posizione[0]
-
-        intersezione = np.array([x_int, y_int, z_int])
-
-        if np.sum(((intersezione-centro)**2)) < superficie.raggio:
-            print("intersecato")
-            return intersezione
-        else:
-            print("non intersecato")
+        d2 = np.sum((pts - centro)**2, axis=1)
+        mask = d2 < superficie.raggio**2
+        if len(pts[mask])==0:
             return None
+        xs, ys, zs = pts[mask].T
+        phi, psi = phi[mask].T, psi[mask].T
+        if scatter_compton:
+            scatter_angle = scatter_angle[mask].T
 
+        if debug_graph:
+            fig = plt.figure(figsize=(12, 12))
+            ax = fig.add_subplot(projection='3d')
+            ax.scatter(xs,ys,zs) 
+            plt.show()
+        if not scatter_compton:
+            scatter_angle = np.full(len(xs),None)
+        return xs,ys,zs, np.degrees(phi), np.degrees(psi), scatter_angle
 
 ######## Funzioni ########
 
@@ -120,114 +155,6 @@ def campiona_kn(theta_mesh, E, N):
     x = inv_cdf(u)
     return x
 
-def calcola_angolo_cristallo(phi, dbc, dsp):
-    """"" Trova l'angolo tra la "sorgente" e il centro del detector, usato ai fini dell'accettanza
-
-    Parametri:
-    phi: angolo in gradi sul goiniometro del cristallo
-    dbc: distanza bersaglio-cristallo
-    dsp: distanza sorgente-plastico
-
-    Returns: l'angolo in radianti
-    """""
-    phi = np.radians(phi)
-    l = np.sqrt(dsp**2+dbc**2-(2*dsp*dbc*np.cos(np.pi-phi)))
-    angolo = np.arccos((dsp-(dbc*np.cos(np.pi-phi)))/l)
-    return angolo
-
-def distribuzione_sorgente(n, type=0):
-    """"" Funzione che seleziona la distribuzione della sorgente di Co
-
-    Parametri: 
-    n: Numero di fotoni da generare
-    type: Tipo di sorgente. 0 = uniforme, 1 = gaussiana, 2 = lineare
-
-    Returns:
-    array di angoli in radianti generati dalla distribuzione
-    """""
-    if type==0:
-        theta_in = np.radians(np.random.uniform(-4.75, 4.75, n)) # radianti
-    elif type==1:
-        theta_in = np.radians(np.random.normal(0,4.75/3, n)) # radians
-    elif type==2:
-        theta_in = np.full(n, 0)
-    else:
-        print("devo ancora implementare altre cose")
-    return theta_in
-
-def scatter(E, n):
-    """"" Funzione che calcola l'angolo di scattering Compton
-
-    Parametri: 
-    E: Energia del fotone incidente [keV]
-    n: Numero di fotoni da scatterare
-
-    Returns:
-    theta_scattered: L'angolo di scattering Compton (radianti)
-    theta_total: L'angolo calcolato dalla sorgente ai fini di contare se arriva sul cristallo (radianti)
-    """""
-    theta_in = distribuzione_sorgente(n, type=0) # radianti (0: uniforme, 1: gauss, 2: ideale)
-    theta_scattered = campiona_kn(THETA_MESH, E, n) # radianti
-    theta_total = theta_in + theta_scattered
-    return theta_scattered, theta_total
-
-def accept(E):
-    """"" Funzione che calcola se un raggio viene visto dal cristallo o no
-
-    Parametri:
-    theta_scattered: numpy array di angoli di scattering
-    theta_total: numpy array di angoli misurati dalla sorgente
-
-    Returns:
-    numpy array di angoli di scattering considerando solo quelli visti dal cristallo
-    """""
-    theta_scattered, theta_total = scatter(E, N_MC)
-    thetas_accepted=[]
-    counter = 0
-    for i in theta_total:
-        if (i>np.radians(PHI) - SAAC) and (i<np.radians(PHI) + SAAC):
-            thetas_accepted.append(theta_scattered[counter])
-        counter += 1
-    thetas_accepted = np.degrees(np.array(thetas_accepted))
-    return thetas_accepted
-
-def seen_thetas(picco=1):
-    """"" Genera il grafico per gli angoli di scattering visti in base al picco
-
-    Parametri:
-    picco: 1 o 2 in base al picco che si vuole vedere
-
-    Returns: None
-    """""
-    if picco == 1:
-        thetas_accepted=accept(E1)
-    if picco == 2:
-        thetas_accepted=accept(E2)
-    else:
-        print("Picco deve essere 1 o 2")
-        end
-
-    fotoni_visti = len(thetas_accepted)
-    print(f'\n ========== RISULTATI ==========')
-    print(f'Abbiamo visto {round(fotoni_visti,2)} fotoni su {int(N_MC)}, ({round(100 * fotoni_visti/N_MC,2)}%)')
-    print(f'Per vederne {STAT_DES} servierebbero {round(STAT_DES*N_MC/(FLUSSO*60*fotoni_visti),2)} minuti')
-    print(f'Angoli che vediamo: [{round(min(thetas_accepted),2)}, {round(max(thetas_accepted),2)}] gradi, con il cristallo centrato a {PHI} gradi')
-    print(f'Delta theta = {round(max(thetas_accepted)-min(thetas_accepted),2)} gradi')
-    print(f' ===============================\n')
-
-
-    counts=plt.hist(thetas_accepted, bins=30)
-    plt.axvline(PHI-np.degrees(SAAC), color="red", linestyle="--", label="Detector aperature")
-    plt.axvline(PHI+np.degrees(SAAC), color="red", linestyle="--")
-    half_max = max(counts[0]) / 2
-    indices = np.where(counts[0] >= half_max)[0]
-    fwhm = counts[1][indices[-1]+1] - counts[1][indices[0]]
-    plt.axvline(counts[1][indices[0]], color="black", linestyle="--", label="Full width half maximum")
-    plt.axvline(counts[1][indices[-1]+1], color="black", linestyle="--")
-    plt.axvline(np.mean(thetas_accepted), color="yellow", label=f"Mean angle: {round(np.mean(thetas_accepted),2)}")
-    plt.legend()
-    plt.show()
-
 def compton(E, theta, errore=0):
     """"" Calcola l'energia di un fotone entrante con energia E ed angolo theta
 
@@ -241,43 +168,64 @@ def compton(E, theta, errore=0):
     """""
     return E/(1+(E*(1-np.cos(theta))/ME)) + np.random.normal(0,errore,1)
 
-def plot_compton():
-    thetas_accepted1 = accept(E1)
-    thetas_accepted2 = accept(E2)
+def mc(E, phi_cristallo=PHI):
+    sorgente    = Superficie(1,(0,0,-DSP-L),0)
+    collimatore = Superficie(1,(0,0,-DSP), 0)
+    plastico    = Superficie(4, (0,0,0), 0)
+    cristallo   = Superficie(RC,(0,DBC*np.sin(np.radians(phi_cristallo)), DBC*np.cos(np.radians(phi_cristallo))), phi_cristallo)
 
+    ## Sorgente - collimatore
+    xs, ys, zs = sorgente.pos_sul_piano_unif(N_MC, debug_graph=False) # Genera N punti uniformi sulla sorgente
+    phiphi, psipsi = np.random.uniform(-SAACOLL, SAACOLL, len(xs)), np.random.uniform(-SAACOLL, SAACOLL, len(xs)) # Genera angoli uniformi
+    f = Fotone(E, xs, ys, zs, phiphi, psipsi) # Genera fotoni 
+    xc, yc, zc, phis, psis, _ = f.calcola_int(collimatore, debug_graph=False) # Trova intersezione con collimatore
+    #print(f"{round(100*len(xc)/len(xs),2)}% dei fotoni generati escono dal collimatore")
+
+    # Collimatore - plastico
+    f = Fotone(E, xc, yc, zc, phis, psis) # Fotoni sul collimatore con l'angolo da prima
+    xp, yp, zp, phip, psip, _ = f.calcola_int(plastico, debug_graph=False) # Trova intersezione con plastico
+    #print(f"{round(100*len(xp)/len(xc),2)}% dei fotoni uscenti colpiscono il plastico")
+
+    # Plastico - cristallo
+    f = Fotone(E, xp, yp, zp, phip, psip)
+    xcr, ycr, zcr, _, _, scatter_angles =  f.calcola_int(cristallo, debug_graph=False, scatter_compton=True)
+    #print(f"{round(100*len(xc)/len(xp),2)}% dei fotoni dal plastico colpiscono il cristallo")
     
-    compton1=compton(E1, np.radians(thetas_accepted1),5)
-    compton2=compton(E2, np.radians(thetas_accepted2),5)
+    #print(f"{round(100*len(xcr)/len(xs),2)}% dei fotoni generati colpiscono il cristallo")
+    print(f"{round(100*len(xcr)/len(xc),2)}% dei fotoni che escono dal collimatore colpiscono il cristallo")
+    #plt.hist(np.degrees(scatter_angles), bins=np.linspace(-90,90,80), label=E, histtype="step")
 
-    sommato=np.concatenate((compton1, compton2))
-    b_min = min(compton1)
-    b_max = max(compton2)
-    binss=np.linspace(b_min, b_max, 50)
+    print(f"{round((STAT_DES*len(xp))/(FLUSSO*len(xcr)*3600),2)} ore per avere {STAT_DES} eventi")
+    energie = compton(E, scatter_angles, 5)
+    #plt.hist(energie, bins=np.linspace(energie.min(), energie.max(), 40), label=E, histtype="step")
 
-    #plt.hist(compton1, bins=binss, color="red", histtype="step", label=f"Picco a {round(E1,2)}keV")
-    #plt.hist(compton2, bins=binss, color="blue", histtype="step", label=f"Picco a {round(E2,2)}keV")
+    return energie, scatter_angles
+
+def plot_compton(phi_cristallo=PHI, all_peaks=False):
+    energie1, scatter_angles1 = mc(E1, phi_cristallo)
+    energie2, scatter_angles2 = mc(E2, phi_cristallo)
+
+    sommato=np.concatenate((energie1, energie2))
+    b_min = min(energie1)
+    b_max = max(energie2)
+    binss=np.linspace(b_min, b_max, 40)
+
+    if all_peaks:
+        plt.hist(energie1, bins=binss, color="red", histtype="step", label=f"Picco del fotone di {round(E1,1)}keV")
+        plt.hist(energie2, bins=binss, color="blue", histtype="step", label=f"Picco del fotone di {round(E2,1)}keV")
     plt.hist(sommato, bins=binss, color="black", histtype="step", label=f"Somma")
-    plt.axvline(compton(E1, np.radians(np.mean(thetas_accepted1))), color="red", linestyle="--", label=f"{compton(E1, np.radians(np.mean(thetas_accepted1)))}keV")
-    plt.axvline(compton(E2, np.radians(np.mean(thetas_accepted2))), color="blue", linestyle="--", label=f"{compton(E2, np.radians(np.mean(thetas_accepted2)))}keV")
+    plt.axvline(compton(E1, np.mean(scatter_angles1)), color="red",  linestyle="--", label=f"Scattering medio: {round(np.mean(np.degrees(scatter_angles1)),1)} gradi, E={round(compton(E1, np.mean(scatter_angles1))[0],1)}keV")
+    plt.axvline(compton(E2, np.mean(scatter_angles2)), color="blue", linestyle="--", label=f"Scattering medio: {round(np.mean(np.degrees(scatter_angles2)),1)} gradi, E={round(compton(E2, np.mean(scatter_angles2))[0],1)}keV")
+    plt.title(f"Segnale simulato per il cristallo posto a {phi_cristallo} gradi")
     plt.legend()
-    plt.show()
-    pass
+    return sommato
 
 ######## Monte-Carlo ########
 start = time.time()
 
-#plot_compton()
-sorgente    = Superficie(1,(0,0,-DSP-L),0)
-collimatore = Superficie(1,(0,0,-DSP), 0)
-plastico    = Superficie(10, (0,0,0), 0)
-cristallo   = Superficie(RC,(0,DBC*np.sin(np.radians(PHI)), DBC*np.cos(np.radians(PHI))), PHI)
-
-f=Fotone(E1, [0,0,0], [0,0])
-f.calcola_int(cristallo)
+plot_compton(phi_cristallo=20, all_peaks=True)
 
 end = time.time()
 print(f'Tempo impiegato: {round(end - start,2)}s')
 plt.show()
-
-
 
