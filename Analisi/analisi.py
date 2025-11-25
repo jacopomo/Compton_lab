e59852c430ca9e061007b261a556b1694fdb6193
jacopomo <jacopo.omodei@gmail.com>
2025-11-25 18:23:05 +0100

    aggiunto 15gradi

---
+++ Analisi/analisi.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Prendi un .dat file di misure reali
+# Converti conteggi .dat in misure di canale
+# Prendi una calibrazione (quella del giorno?)
+# Calibra canale -> energia
+# Rebinna seguendo un binning predifinito (lo stesso che userò nella simulazione)
+
+# Se richesto, normalizza dividendo in maniera che il conteggio massimo è 1
+
+# Plotta istogramma calibrato e rebinnato
+# Return i conteggi e errori poissoniani sui conteggi
+
+# Dopo questi conteggi verranno confrontati con quelli della simulazione (normalizzati)
