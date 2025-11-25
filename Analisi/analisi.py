c53e351e7a95212b4a4b068c886f8ddf566f23ab
jacopomo <jacopo.omodei@gmail.com>
2025-11-25 22:34:11 +0100

    aggiunto analisi preliminare

--- Analisi/analisi.py
+++ Analisi/analisi.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 # Prendi un .dat file di misure reali
 # Converti conteggi .dat in misure di canale
@@ -12,3 +14,32 @@ import numpy as np
 # Return i conteggi e errori poissoniani sui conteggi
 
 # Dopo questi conteggi verranno confrontati con quelli della simulazione (normalizzati)
+
+
+
+gradi = input("A quanti gradi? -> ")
+file = os.path.join("Dati\Measures", f"{gradi}deg.dat")
+
+num_bins = 256
+binning = np.linspace(0,2000, num_bins)
+
+dat = np.loadtxt(file, dtype=int, unpack=True)
+bin_indices = np.arange(dat.size, dtype=int)
+unbinned = np.repeat(bin_indices, dat)
+
+plt.figure(figsize=(12,7), dpi=100)
+
+energie = (unbinned*0.24725)-3.891 ### Calibrazione presa "al volo"
+
+misure = plt.hist(energie, bins=binning, histtype='step', density=True, label="Segnale misurato", color="black")
+
+p1, p2, tot = np.loadtxt(os.path.join("Montecarlo\Simulazioni", f"simul_dati_{gradi}gradi.csv"), delimiter=",").T
+simul = plt.hist(tot, bins=binning, histtype='step', density=True, label="Segnale simulato", color="red")
+
+plt.title(f"Analisi dello spettro a {gradi} gradi")
+plt.xlabel(f'Energia [keV]')
+plt.ylabel('Conteggi')
+plt.legend()
+file_path = os.path.join("Dati\Measures", f"confronto_{gradi}deg.png")
+plt.savefig(file_path)
+plt.show()
