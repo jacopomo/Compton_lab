ecd63cc1d687780fb71284f3707f59b7fb841019
jacopomo <jacopo.omodei@gmail.com>
2025-11-27 18:25:42 +0100

    aggiunto 35gradi

--- Montecarlo/compton_MC.py
+++ Montecarlo/compton_MC.py
@@ -8,7 +8,7 @@ import time
 import os
 
 ######## Constanti ########
-N_MC = int(5e5) # Num samples (MASSIMO 5e7 SE NON VUOI FAR DIVENTARE IL TUO COMPUTER UN TERMOSIFONE)
+N_MC = int(5e7) # Num samples (MASSIMO 5e7 SE NON VUOI FAR DIVENTARE IL TUO COMPUTER UN TERMOSIFONE)
 
 ### Geometria:
 RCOL = 1 # Raggio collimatore [m]
@@ -53,7 +53,7 @@ class Materiale:
 
     def _build_splines(self):
         """Metodo privato, leggi il file una volta"""
-        ph_E, sigma_c, sigma_pe, sigma_tot = np.loadtxt(f"./Dati_materiali/{self.formula}.txt", skiprows=2, unpack=True)
+        ph_E, sigma_c, sigma_pe, sigma_tot = np.loadtxt(f"./Montecarlo/Dati_materiali/{self.formula}.txt", skiprows=2, unpack=True)
         ph_E = ph_E * 1000  # MeV to keV
 
         self.sigma_c_spline  = CubicSpline(ph_E, sigma_c)
@@ -86,8 +86,6 @@ class Materiale:
         L = np.where(mask, L_pe, L_c)
         interaction = np.where(mask, "Fotoelettrico", "Compton")
 
-        if L.size == 1:
-            return L[0], interaction[0]
         return L, interaction
 
 class Superficie:
@@ -125,7 +123,7 @@ class Volume:
         self.materiale = materiale
     
     def cml(self, E):
-        self.materiale.cml(E)
+        return self.materiale.cml(E)
 
 class Fotone:
     def __init__(self, energia, px, py, pz, phi, psi): #Passa gradi phi psi
@@ -186,13 +184,13 @@ class Fotone:
             scatter_angle = np.full(len(xs),None)
         return xs,ys,zs, np.degrees(phi), np.degrees(psi), scatter_angle
 
-    def isinside(x,y,z, volume):
+    def isinside(self, x,y,z, vol):
         """ Vede se il fotone sta dentro un solido
         Mi raccomando, coordinate x,y,z con z l'asse del prisma e l'origine nel centro della sup vicina, in cm
         """
 
         r = np.sqrt(x**2+y**2)
-        mask = (r < volume.raggio) & (z < volume.lunghezza)
+        mask = (r < vol.raggio) & (z < vol.lunghezza)
         if mask.shape == ():
             return bool(mask)
         return mask
@@ -217,17 +215,18 @@ class Fotone:
         active = np.ones(len(E), dtype=bool) # Fotoni "attivi"
         while np.any(active):
             L, tipo = volume.cml(E[active])
-            new_p = p[active] + d[active] * L[:, np.newaxis]
-            inside = self.isinside(new_p.T, volume)
+            new_p = p[active] + d[active] * L[:, None]
+            inside = self.isinside(new_p[:,0], new_p[:,1], new_p[:,2], volume)
 
+            active_idx = np.where(active)[0]
             escaped = ~inside
             if np.any(escaped):
-                idx_escaped = np.where(active)[0][escaped]
+                idx_escaped = active_idx[escaped]
                 no_deposit = E_depositata[idx_escaped] == 0
                 E_depositata[idx_escaped[no_deposit]] = np.nan
                 active[idx_escaped] = False
 
-            inside_idx = np.where(active)[0][inside]
+            inside_idx = active_idx[inside]
             if len(inside_idx) == 0:
                 break  # no active photons remain
 
@@ -241,23 +240,29 @@ class Fotone:
 
             idx_c = inside_idx[compton_mask]
             if len(idx_c) > 0:
-                deposited = compton(E[idx_c])
-                E_depositata[idx_c] += deposited
-                E[idx_c] -= deposited
+
 
                 # Aggiorna angoli di scattering
-                scatter_angle = campiona_kn(THETA_MESH, E[idx_c], len(idx_c))
-                delta = np.random.uniform(-np.pi/2, np.pi/2)
+                scatter_angle = np.zeros_like(E[idx_c])
+                for i in range(len(E[idx_c])):
+                    scatter_angle[i] = campiona_kn(THETA_MESH, E[idx_c][i], 1)
+                delta = np.random.uniform(-np.pi/2, np.pi/2, size=len(idx_c))
                 phi[idx_c] += scatter_angle * np.cos(delta)
                 psi[idx_c] += scatter_angle * np.sin(delta)
 
                 # Aggiorna posizioni
-                p[idx_c] = new_p[compton_mask]
+                new_p_active = new_p[inside]      # shape = (#inside, 3)
+                new_p_compton = new_p_active[compton_mask]   # shape = (#compton, 3)
+                p[idx_c] = new_p_compton
                 dx = np.sin(psi[idx_c])
                 dy = np.sin(phi[idx_c]) * np.cos(psi[idx_c])
                 dz = np.cos(phi[idx_c]) * np.cos(psi[idx_c])
                 d[idx_c] = np.stack((dx, dy, dz), axis=-1)
 
+
+                deposited = compton(E[idx_c], scatter_angle)
+                E_depositata[idx_c] += deposited
+                E[idx_c] -= deposited
         return E_depositata
         
 
@@ -340,19 +345,18 @@ def mc(E, phi_cristallo=PHI):
 
     print(f"{round((STAT_DES*len(xp))/(FLUSSO*len(xcr)*3600),2)} ore per avere {STAT_DES} eventi")
 
-    # Deposito d'energia dentro il cristallo
-    #f = Fotone(np.full(len(xcr), E), xcr, ycr, zcr, phicr, psicr)
-    #energie = f.scatter_inside(PMT2) 
-
     energie = compton(E, scatter_angles, 5)
-    #plt.hist(energie, bins=np.linspace(energie.min(), energie.max(), 40), label=E, histtype="step")
+    # Deposito d'energia dentro il cristallo
+    f = Fotone(energie, xcr, ycr, zcr, phicr, psicr)
+    energia_depo = f.scatter_inside(PMT2) 
 
-    return energie, scatter_angles
+    #plt.hist(energie, bins=np.linspace(energie.min(), energie.max(), 40), label=E, histtype="step")
+    
+    return energia_depo, scatter_angles
 
 def plot_compton(phi_cristallo=PHI, plot_scatter_angles=False, all_peaks=False):
     energie1, scatter_angles1 = mc(E1 ,phi_cristallo)
     energie2, scatter_angles2 = mc(E2, phi_cristallo)
-
     if plot_scatter_angles:
         angoli = np.concatenate((np.degrees(scatter_angles1),np.degrees(scatter_angles2)))
         plt.figure(figsize=(12,7), dpi=100)
@@ -366,15 +370,13 @@ def plot_compton(phi_cristallo=PHI, plot_scatter_angles=False, all_peaks=False):
         pass
 
     sommato=np.concatenate((energie1, energie2))
-    b_min = min(energie1)
-    b_max = max(energie2)
-    binss=np.linspace(b_min, b_max, 40)
+    binss = np.linspace(0,2000, 80)
 
     plt.figure(figsize=(12,7), dpi=100)
     if all_peaks:
-        plt.hist(energie1, bins=binss, color="red", histtype="step", label=f"Picco del fotone di {round(E1,1)}keV")
-        plt.hist(energie2, bins=binss, color="blue", histtype="step", label=f"Picco del fotone di {round(E2,1)}keV")
-    plt.hist(sommato, bins=binss, color="black", histtype="step", label=f"Somma")
+        plt.hist(energie1, bins=binss, color="red", histtype="step", label=f"Picco del fotone di {round(E1,1)}keV", density=False)
+        plt.hist(energie2, bins=binss, color="blue", histtype="step", label=f"Picco del fotone di {round(E2,1)}keV", density=False)
+    plt.hist(sommato, bins=binss, color="black", histtype="step", label=f"Somma", density=False)
     plt.axvline(compton(E1, np.mean(scatter_angles1)), color="red",  linestyle="--", label=f"Scattering medio: {round(np.mean(np.degrees(scatter_angles1)),1)} gradi, E={round(compton(E1, np.mean(scatter_angles1))[0],1)}keV")
     plt.axvline(compton(E2, np.mean(scatter_angles2)), color="blue", linestyle="--", label=f"Scattering medio: {round(np.mean(np.degrees(scatter_angles2)),1)} gradi, E={round(compton(E2, np.mean(scatter_angles2))[0],1)}keV")
     plt.title(f"Segnale simulato per il cristallo posto a {phi_cristallo} gradi")
@@ -393,7 +395,7 @@ def plot_compton(phi_cristallo=PHI, plot_scatter_angles=False, all_peaks=False):
 ######## Monte-Carlo ########
 start = time.time()
 
-plot_compton(phi_cristallo=15, plot_scatter_angles=True, all_peaks=True)
+plot_compton(phi_cristallo=15, plot_scatter_angles=False, all_peaks=True)
 
 
 end = time.time()
